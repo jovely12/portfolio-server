c854c56737c541af4c9edeca69bd32cc4f771e36
jovely12 <jovely12@naver.com>
2024-04-10 14:03:41 +0900

    'aaa'

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 const credentials = require('./middleware/credentials');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 8000;
 
 // Connect to MongoDB
 connectDB();
@@ -61,5 +61,5 @@ app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
 	console.log('Connected to MongoDB');
-	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+	app.listen(PORT,"3.37.56.48", () => console.log(`Server running on port ${PORT}`));
 });
